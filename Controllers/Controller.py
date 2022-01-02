1aa046b5ac650349768c44e37d4231fee47bd1a2
S. Ulaşcan Kılıç <ulascank1@gmail.com>
2022-01-02 17:34:21 +0300

    cors

--- Controllers/Controller.py
+++ Controllers/Controller.py
@@ -58,7 +58,13 @@ class Gotten(Resource):
                 }
             
         }
+@app.after_request
 
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  return response      
 
 api.add_resource(Hello, "/<string:str>")
 api.add_resource(Print, "/print") 
